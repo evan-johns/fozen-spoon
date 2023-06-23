bdf653ebc352a1ec3cad19fc06a85553f0aca8e9
evan-johns <evandjohns@gmail.com>
2023-06-23 17:20:39 -0400

    updated nav with svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Hero from "./components/Hero";
+import Nav from "./components/Nav";
 
 const HomePage: React.FC = () => {
   return (
     <div>
-      <Hero />
+      <Nav />
     </div>
   );
 };
