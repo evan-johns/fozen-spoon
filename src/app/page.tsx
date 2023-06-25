4d5682f4ec1069b48a5a97397bf0c857423eb11a
evan-johns <evandjohns@gmail.com>
2023-06-24 22:57:58 -0400

    added footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <Nav />
+      <Footer />
     </div>
   );
 };
